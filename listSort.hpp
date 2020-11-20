d9b76a90cf2a3f086acf7c188e93a26b50f7a859
aramismas <aramismasarati@yahoo.com>
2020-11-19 22:20:47 -0800

    Added listContainer and Sort function

---
+++ listSort.hpp
@@ -0,0 +1,13 @@
+#ifndef SORTLIST_HPP
+#define SORTLIST_HPP
+
+#include "sort.hpp"
+#include "container.hpp"
+
+class ListSort {
+    public:
+	ListSort();
+        void sort(Container* container);
+};
+
+#endif // SORTLIST_HPP
