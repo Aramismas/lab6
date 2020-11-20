eff154689ad37bea03c656d041f8847de48a037f
arzhangv <34948438+arzhangv@users.noreply.github.com>
2020-11-20 10:42:54 -0800

    Update listContainerTest.hpp

--- listContainerTest.hpp
+++ listContainerTest.hpp
@@ -9,7 +9,7 @@
 #include "pow.h"
 #include "listContainer.hpp" 
 #include "selectionSort.hpp"
-
+#include "BubbleSort.h"
 TEST(ListContainerTestSet, SwapTest) {
     Op* seven = new Op(7);
     ListContainer* test_container = new ListContainer();
@@ -87,3 +87,35 @@ TEST(SortTestSet, SelectionSortTest) {
     EXPECT_EQ(container->at(1)->evaluate(), 5);
     EXPECT_EQ(container->at(2)->evaluate(), 28);
 }
+TEST(BubbleSortTestSet, BubbleSortTest) {
+    Op* seven = new Op(7);
+    Op* four = new Op(4);
+    Mult* TreeA = new Mult(seven, four);
+
+    Op* three = new Op(3);
+    Op* two = new Op(2);
+    Add* TreeB = new Add(three, two);
+
+    Op* ten = new Op(10);
+    Op* six = new Op(6);
+    Sub* TreeC = new Sub(ten, six);
+
+    ListContainer* container = new ListContainer();
+    container->add_element(TreeA);
+    container->add_element(TreeB);
+    container->add_element(TreeC);
+
+    ASSERT_EQ(container->size(), 3);
+    EXPECT_EQ(container->at(0)->evaluate(), 28);
+    EXPECT_EQ(container->at(1)->evaluate(), 5);
+    EXPECT_EQ(container->at(2)->evaluate(), 4);
+
+    container->set_sort_function(new BubbleSort());
+    container->sort();
+
+    ASSERT_EQ(container->size(), 3);
+    EXPECT_EQ(container->at(0)->evaluate(), 4);
+    EXPECT_EQ(container->at(1)->evaluate(), 5);
+    EXPECT_EQ(container->at(2)->evaluate(), 28);
+}
+
